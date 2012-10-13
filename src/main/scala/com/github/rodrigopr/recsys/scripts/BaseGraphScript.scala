b9005366b684d94ba2ae84d6bd1f60e561685bb6
Rodrigo Ribeiro <rodriguinho@jusbrasil.com.br>
2012-10-13 02:19:37 -0300

    Updating neo4j version, there was a bug in version 1.8. Cypher was randomly crashing in multi threading environment

--- src/main/scala/com/github/rodrigopr/recsys/scripts/BaseGraphScript.scala
+++ src/main/scala/com/github/rodrigopr/recsys/scripts/BaseGraphScript.scala
@@ -7,13 +7,16 @@ import org.neo4j.graphdb.GraphDatabaseService
 class BaseGraphScript extends App {
   System.setProperty("org.neo4j.rest.stream", "true")
 
-  val graphDB = new RestGraphDatabase("http://tccvps:7474/db/data/")
+  val graphDB = new RestGraphDatabase("http://localhost:7474/db/data/")
   val engine = new ExecutionEngine(graphDB)
 
   val indexMovie = graphDB.index().forNodes("movie")
   val indexGenre = graphDB.index().forNodes("genre")
   val indexUser = graphDB.index().forNodes("user")
   val indexCluster = graphDB.index().forNodes("cluster")
+  val indexInCLuster = graphDB.index().forNodes("inCluster")
+  val indexNeighbor = graphDB.index().forRelationships("neighbor")
+  val indexNeighborWithCluster = graphDB.index().forRelationships("neighborWithCluster")
 
   registerShutdownHook(graphDB)
 
@@ -26,6 +29,7 @@ class BaseGraphScript extends App {
   }
 
   def doTx[T](f: GraphDatabaseService => T) : T = {
+    // not used transaction cause it was causing problem with the rest client for neo4j
     //val tx = graphDB.beginTx()
     try {
       f(graphDB)
