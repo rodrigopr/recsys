b7a9186348974f23159fcf7dc0512fa2c0e503fa
Rodrigo Ribeiro <rodriguinho@jusbrasil.com.br>
2012-10-22 01:20:59 -0300

    Fix GenreFeature extration in clustering. Refactored clusterBuilder task.

--- src/main/scala/com/github/rodrigopr/recsys/clusters/GenreFeature.scala
+++ src/main/scala/com/github/rodrigopr/recsys/clusters/GenreFeature.scala
@@ -7,7 +7,7 @@ import math._
 import com.github.rodrigopr.recsys.utils.Memoize
 
 object GenreFeature extends ClusterFeature {
-  def getMovieGenreMemoized = Memoize.memoize((movieId: String) => {
+  val getMovieGenreMemoized = Memoize.memoize((movieId: String) => {
     pool.withClient{ client =>
       client.smembers[String](buildKey("movie", movieId, "genres")).getOrElse(Set[Option[String]]()).map(_.get)
     }
@@ -22,27 +22,28 @@ object GenreFeature extends ClusterFeature {
   val extractFeaturesMemoized = Memoize.memoize((user: String) => {
     Console.println("Calculating item: " + user)
 
-    val interestMap = mutable.HashMap[String, Double]()
-
     val ratings = pool.withClient(
       _.zrangeWithScore(buildKey("ratings", "user", user), 0)
     ).get.map{ case(movieId, rating) => (movieId, rating, getMovieGenreMemoized(movieId)) }
+
+    // count all ratings the user made for each genre
     val countGenres = processGenre(genre => ratings.count(_._3.contains(genre))).toMap
 
+    // get the average ratings the user made for each genre
     val avgGenres = processGenre(genre => ratings.filter(_._3.contains(genre)).map(_._2).avg).toMap
 
-    for (genre <- allGenres) {
+    val interestMap = allGenres.filter(g => countGenres(g.id) > 0).map { genre =>
       val totalCategory = countGenres.getOrElse(genre.id, 0)
       val avgRatingCat = avgGenres.getOrElse(genre.id, 0.0d)
 
       // Get the interest coefficient for the genre
       val likeFactor = log(1 + totalCategory) * pow(avgRatingCat, 2)
-      interestMap.put(genre.id, likeFactor)
-    }
+      genre.id -> likeFactor
+    }.toMap
 
     val max = interestMap.values.max
     val min = interestMap.values.min
 
-    interestMap.map{ case(id, likelihood) => (id, (likelihood - min) / (max - min)) }.asInstanceOf[Map[String, Double]]
+    interestMap.map{ case(id, likelihood) => (id, (likelihood - min) / (max - min)) }
   })
 }
--- src/main/scala/com/github/rodrigopr/recsys/tasks/ClusterBuilder.scala
+++ src/main/scala/com/github/rodrigopr/recsys/tasks/ClusterBuilder.scala
@@ -13,28 +13,61 @@ import com.github.rodrigopr.recsys.utils.RedisUtil._
 import com.github.rodrigopr.recsys.clusters.{GenreFeature, ClusterFeature}
 
 object ClusterBuilder extends Task {
+  private var attributesMap: Map[String, Attribute] = _
+  private val userFeatures = mutable.Map[String, Map[String, Double]]()
 
   def execute(config: Config) = {
-    collection.parallel.ForkJoinTasks.defaultForkJoinPool.setParallelism(config.getInt("parallelSize"))
-    val numClusters = config.getInt("numClusters")
+    collection.parallel.ForkJoinTasks.defaultForkJoinPool.setParallelism(config.getInt("parallelism"))
+    val numClusters = config.getInt("num-clusters")
 
     val features: Map[String, ClusterFeature] = Map(
       "genre" -> GenreFeature
     )
+
     val usedFeatures = config.getStringList("features").map(features.getOrElse(_, null)).filter(_ != null)
 
     val attributesName = usedFeatures.map(_.getFeatureList).flatten
     val totalAttributes = attributesName.size
 
     val ids = 0.to(totalAttributes).iterator
-    val attributesMap = attributesName.map(a => a -> new Attribute(a, ids.next())).toMap
+    attributesMap = attributesName.map(a => a -> new Attribute(a, ids.next())).toMap
 
+    val dataset: Instances = generateDataset(usedFeatures)
+
+    val cluster = executeCluster(dataset, numClusters, config)
+
+    assignClusters(cluster, dataset)
+
+    groupClusterRatings(numClusters)
+
+    true
+  }
+
+  def assignClusters(cluster: Clusterer, dataset: Instances) {
+    // for each user create a relationship with his calculated cluster
+    userFeatures.foreach {
+      case (userId, featureData) =>
+        val instance = getInstance(featureData)
+        instance.setDataset(dataset)
+
+        // calculate the best cluster for the item
+        val clusterNum = cluster.clusterInstance(instance)
+
+        // save in redis
+        pool.withClient(_.pipeline {
+          client =>
+            client.zadd(buildKey("cluster", clusterNum.toString), 0.0d, userId)
+            client.set(buildKey("user", userId, "cluster"), clusterNum.toString)
+        })
+    }
+  }
+
+  def generateDataset(usedFeatures: Seq[ClusterFeature]): Instances = {
     // create the dataset with the feature list pre calculated
     val dataset = new Instances("data", new util.ArrayList[Attribute](attributesMap.values), 0)
 
     // fetch all users from redis
-    val usersIds = pool.withClient(client => client.smembers("users")).get.map(_.get)
-    val userFeatures = mutable.Map[String, Map[String, Double]]()
+    val usersIds = pool.withClient(_.smembers("users")).get.map(_.get)
 
     // in parallel calculate the feature data for each list
     val instances = usersIds.par.map( user => {
@@ -44,47 +77,33 @@ object ClusterBuilder extends Task {
       // salve the feature map for future use
       userFeatures.put(user, allFeatures)
 
-      getInstance(allFeatures, totalAttributes, attributesMap)
+      getInstance(allFeatures)
     })
 
     // add each instance to the dataset
-    instances.foreach(dataset.add)
+    instances.seq.foreach(dataset.add)
+    dataset
+  }
 
-    val cluster = executeCluster(dataset, numClusters, config)
-
-    // for each user create a relationship with his calculated cluster
-    userFeatures.foreach { case (userId, featureData) =>
-      val instance = getInstance(featureData, totalAttributes, attributesMap)
-      instance.setDataset(dataset)
-
-      // calculate the best cluster for the item
-      val clusterNum = cluster.clusterInstance(instance)
-
-      // save in redis
-      pool.withClient ( _.pipeline { client =>
-        client.zadd(buildKey("cluster", clusterNum.toString), 0.0d, userId)
-        client.set(buildKey("user", userId, "cluster"), clusterNum.toString)
-      })
-    }
-
+  def groupClusterRatings(numClusters: Int) {
     // group ratings per cluster
     pool.withClient(_.smembers("movies")).get.par.foreach { movieId =>
-      0.to(numClusters-1).foreach { clusterNum =>
+
+      0.to(numClusters - 1).foreach {clusterNum =>
         val keyDest = buildKey("ratings", "movie", movieId.get, "cluster", clusterNum.toString)
         val keyRatingsMovie = buildKey("ratings", "movie", movieId.get)
         val keyCluster = buildKey("cluster", clusterNum.toString)
+
         pool.withClient(_.zinterstore(keyDest, List(keyRatingsMovie, keyCluster), MAX))
       }
     }
-
-    true
   }
 
   /**
-   * transform the feature map in a weka's SparceInstance
+   * transform the feature map in a WEKA's SparseInstance
    */
-  def getInstance(featureMap: Map[String, Double], totalAttributes: Int, attributesMap: Map[String, Attribute]): SparseInstance = {
-    val instance = new SparseInstance(totalAttributes)
+  def getInstance(featureMap: Map[String, Double]): SparseInstance = {
+    val instance = new SparseInstance(attributesMap.size)
     featureMap.foreach { case (name, rating) =>
       instance.setValue(attributesMap.get(name).get, rating)
     }
